434991825cacf863cc9d961347951d8b088d3e7a
Jacob Januszanis <Jjanuszanis94@gmail.com>
2020-12-30 14:20:24 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="homepage">
+        <h1 className='text-white display-1'>Jakes Portfolio</h1>
+      </div>
     </div>
   );
 }
