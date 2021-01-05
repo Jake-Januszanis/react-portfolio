bf5c511268b4786a186de1c57bd62e5dc06396d0
Jacob Januszanis <Jjanuszanis94@gmail.com>
2021-01-05 12:30:51 -0700

    Deleted bootstrap/reactstrap libraries. Updated design using Sass

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import { Button } from 'reactstrap';
+import './css/App.scss';
+
 
 function App() {
   return (
     <div className="App">
-        <div className="main-container text-white">
-          <h1 className="py-5">Jake Januszanis</h1>
-          <h2>Full Stack Web Developer</h2>
-          <div className="d-flex flex-row justify-content-around">
-          <p>Projects</p>
-          <p>About</p>
-          <p>Contact</p>
-          <p>Resume</p>
+    
+        <div className="main-container">
+          <h1 className="main-container__title-primary">Jake Januszanis</h1>
+          <h2 className="main-container__title-sub">Full Stack Web Developer</h2>
+          <div className="divider"></div>
+          <div className="main-container__links">
+          <span className="main-container__links-text">Projects</span>
+          <span className="main-container__links-text">About</span>
+          <span className="main-container__links-text">Contact</span>
+          <span className="main-container__links-text">Resume</span>
           </div>
-        </div>
+      </div>
       </div>
     
   );
