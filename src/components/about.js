65144a73b134792c00c156696cbcd09256e2b119
Jacob Januszanis <Jjanuszanis94@gmail.com>
2021-01-20 10:31:40 -0700

    Updated about/contact modal & added react icons to page

--- src/components/about.js
+++ src/components/about.js
@@ -1,13 +1,26 @@
 
-import '../css/about.scss';
+
 import '../css/modals.scss';
+import '../css/about.scss';
+import {FaJsSquare, FaReact, FaNode, FaHtml5, FaSass } from 'react-icons/fa'; 
 
 function About ({display, close}) {
     if (display === true) {
         return (
             <div className="modal-show">
-                About Modal
                 <button onClick={close} name="about">Close</button>
+                <div className="about-modal">
+                <h1 className="about-modal__title">About Me</h1>
+                <p className="about-modal__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultrices magna ut tristique pulvinar. Nam bibendum venenatis velit, vitae congue metus efficitur et. Vestibulum dolor lectus, tempor condimentum dui a, molestie pretium ligula. Sed feugiat eros at lacus malesuada, vel elementum sapien dapibus. Aliquam erat volutpat. Sed quis iaculis magna.</p>
+                
+                <div className="about-modal__icons-container">
+                   <FaJsSquare className='about-modal__icons' style={{color: '#fcdc00'}} />
+                   <FaReact className='about-modal__icons' style={{color: '#61dafb'}} />
+                   <FaNode className='about-modal__icons' style={{color: "#43853d"}}/>
+                   <FaHtml5 className='about-modal__icons' style={{color: '#f06529'}}/>
+                   <FaSass className='about-modal__icons' style={{ color: '#bf4080'}}/>
+                </div>
+                </div>
             </div>
         ) 
     } else {
--- src/components/contact.js
+++ src/components/contact.js
@@ -6,8 +6,32 @@ const Contact = ({display, close}) => {
     if (display === true) {
         return (
             <div className="modal-show">
-                Projects Modal
+
+            <div className="contact-container">
                 <button onClick={close} name="contact">Close</button>
+
+                <h1 className="contact-container__title">Lets work together</h1>
+
+                <form className="contact-form">
+                    
+                    <div className="contact-form-holder">
+                        <input type="email" className="contact-form-input" required></input>
+                        <span className="contact-form-label">Email:</span>
+                    </div>
+
+                    <div className="contact-form-holder">
+                        <input type="text" className="contact-form-input" required></input>
+                        <span className="contact-form-label">Full Name:</span>
+                    </div>
+
+                    <div className="contact-form-holder">
+                        <textarea className="contact-form-textarea" rows="5" cols="33" required></textarea>
+                        <span className="contact-form-label">Message:</span>
+                    </div>
+                    <button className="submit-button" type="submit">Send</button>
+                </form>
+                
+            </div>
             </div>
         ) 
     } else {
